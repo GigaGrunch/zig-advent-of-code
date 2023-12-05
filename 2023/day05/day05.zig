9b39dc3f841cb91dc2f45cd57587c826de203907
GigaGrunch <gigagrunch@gmail.com>
2023-12-05 09:57:11 +0100

    store maps with from/to

--- 2023/day05/day05.zig
+++ 2023/day05/day05.zig
@@ -6,28 +6,28 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    _ = allocator;
+    var maps = std.ArrayList(Map).init(allocator);
+    defer maps.deinit();
 
     var lines_it = utils.tokenize(text, "\r\n");
     while (lines_it.next()) |line| {
-        std.debug.print("line: {s}\n", .{line});
-
         if (utils.contains(line, ':')) {
             var parts_it = utils.tokenize(line, ":");
             const prefix = parts_it.next().?;
-            std.debug.print("  prefix: {s}\n", .{prefix});
 
             if (utils.endsWith(prefix, "map")) {
                 var prefix_it = utils.tokenize(prefix, " ");
                 const map_name = prefix_it.next().?;
-                std.debug.print("    map: {s}\n", .{map_name});
 
                 var name_it = utils.tokenize(map_name, "-");
                 const from = name_it.next().?;
-                std.debug.print("      from: {s}\n", .{from});
                 std.debug.assert(utils.streql(name_it.next().?, "to"));
                 const to = name_it.next().?;
-                std.debug.print("      to: {s}\n", .{to});
+
+                try maps.append(.{
+                    .from = from,
+                    .to = to,
+                });
             } else if (utils.streql(prefix, "seeds")) {
                 // seeds
             } else {
@@ -38,8 +38,17 @@ fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
         }
     }
 
+    for (maps.items) |map| {
+        std.debug.print("{s} -> {s}\n", .{ map.from, map.to });
+    }
+
     return 0;
 }
+
+const Map = struct {
+    from: []const u8,
+    to: []const u8,
+};
 
 test {
     const text = @embedFile("example.txt");
