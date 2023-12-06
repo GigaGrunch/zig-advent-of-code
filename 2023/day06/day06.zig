3c97690022cc5f97562d8db2e586d5e052f135e8
GigaGrunch <gigagrunch@gmail.com>
2023-12-06 11:55:31 +0100

    parse values

--- 2023/day06/day06.zig
+++ 2023/day06/day06.zig
@@ -6,8 +6,23 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    _ = allocator;
-    _ = text;
+    var times = std.ArrayList(i32).init(allocator);
+    defer times.deinit();
+    var records = std.ArrayList(i32).init(allocator);
+    defer records.deinit();
+
+    var lines_it = utils.tokenize(text, "\r\n");
+
+    const times_line = lines_it.next().?;
+    var times_it = utils.tokenize(times_line, " ");
+    std.debug.assert(utils.streql(times_it.next().?, "Time:"));
+    while (times_it.next()) |time| try times.append(try utils.parseInt(time));
+
+    const records_line = lines_it.next().?;
+    var records_it = utils.tokenize(records_line, " ");
+    std.debug.assert(utils.streql(records_it.next().?, "Distance:"));
+    while (records_it.next()) |record| try records.append(try utils.parseInt(record));
+
     return 0;
 }
 
