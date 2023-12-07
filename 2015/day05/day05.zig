64d7caa978e58c08d5c26b61d8cb619233afa14b
GigaGrunch <gigagrunch@gmail.com>
2023-12-07 17:11:21 +0100

    find double pairs

--- 2015/day05/day05.zig
+++ 2015/day05/day05.zig
@@ -6,41 +6,35 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    _ = allocator;
-
     var nice_count: i32 = 0;
 
     var lines_it = utils.tokenize(text, "\r\n");
     while (lines_it.next()) |line| {
-        if (isNice(line)) nice_count += 1;
+        if (try isNice(line, allocator)) nice_count += 1;
     }
 
     return nice_count;
 }
 
-fn isNice(text: []const u8) bool {
-    var vowel_count: i32 = 0;
-    var double_count: i32 = 0;
+fn isNice(text: []const u8, allocator: std.mem.Allocator) !bool {
+    var has_double_pair = false;
+    var pairs = std.ArrayList([]const u8).init(allocator);
+    defer pairs.deinit();
 
-    for (0..text.len) |i| {
-        switch (text[i]) {
-            'a', 'e', 'i', 'o', 'u' => vowel_count += 1,
-            else => {},
-        }
-
-        if (i > 0) {
+    for (1..text.len) |i| {
+        if (!has_double_pair) {
             const pair = text[i - 1 .. i + 1];
-
-            if (utils.streql(pair, "ab")) return false;
-            if (utils.streql(pair, "cd")) return false;
-            if (utils.streql(pair, "pq")) return false;
-            if (utils.streql(pair, "xy")) return false;
-
-            if (pair[0] == pair[1]) double_count += 1;
+            for (pairs.items) |other| {
+                if (utils.streql(pair, other) and &pair[0] != &other[1]) {
+                    has_double_pair = true;
+                    break;
+                }
+            }
+            try pairs.append(pair);
         }
     }
 
-    return vowel_count >= 3 and double_count >= 1;
+    return has_double_pair;
 }
 
 test "qjhvhtzxzqqjkmpb" {
