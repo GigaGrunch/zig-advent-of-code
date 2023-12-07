b484af96daf8dd5b6e559ac70914e22e58026f7e
GigaGrunch <gigagrunch@gmail.com>
2023-12-07 17:02:35 +0100

    get count of nice strings

--- 2015/day05/day05.zig
+++ 2015/day05/day05.zig
@@ -7,8 +7,40 @@ pub fn main() !void {
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
     _ = allocator;
-    _ = text;
-    return 0;
+
+    var nice_count: i32 = 0;
+
+    var lines_it = utils.tokenize(text, "\r\n");
+    while (lines_it.next()) |line| {
+        if (isNice(line)) nice_count += 1;
+    }
+
+    return nice_count;
+}
+
+fn isNice(text: []const u8) bool {
+    var vowel_count: i32 = 0;
+    var double_count: i32 = 0;
+
+    for (0..text.len) |i| {
+        switch (text[i]) {
+            'a', 'e', 'i', 'o', 'u' => vowel_count += 1,
+            else => {},
+        }
+
+        if (i > 0) {
+            const pair = text[i - 1 .. i + 1];
+
+            if (utils.streql(pair, "ab")) return false;
+            if (utils.streql(pair, "cd")) return false;
+            if (utils.streql(pair, "pq")) return false;
+            if (utils.streql(pair, "xy")) return false;
+
+            if (pair[0] == pair[1]) double_count += 1;
+        }
+    }
+
+    return vowel_count >= 3 and double_count >= 1;
 }
 
 test "ugknbfddgicrmopn" {
