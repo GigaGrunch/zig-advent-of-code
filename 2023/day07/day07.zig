8f38aee3a47bf4bc122b6b2722137547f3b2b845
GigaGrunch <gigagrunch@gmail.com>
2023-12-07 10:34:26 +0100

    parse hands and bids

--- 2023/day07/day07.zig
+++ 2023/day07/day07.zig
@@ -6,8 +6,22 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    _ = allocator;
-    _ = text;
+    var hands = std.ArrayList([]const u8).init(allocator);
+    defer hands.deinit();
+    var bids = std.ArrayList(i32).init(allocator);
+    defer bids.deinit();
+
+    var lines_it = utils.tokenize(text, "\r\n");
+    while (lines_it.next()) |line| {
+        var parts_it = utils.tokenize(line, " ");
+        try hands.append(parts_it.next().?);
+        try bids.append(try utils.parseInt(i32, parts_it.next().?));
+    }
+
+    for (hands.items, bids.items) |hand, bid| {
+        std.debug.print("{s} {d}\n", .{ hand, bid });
+    }
+
     return 0;
 }
 
