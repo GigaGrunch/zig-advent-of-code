47719b6478ecb0b26370a83bb0c0bdc66b831c3b
GigaGrunch <gigagrunch@gmail.com>
2023-12-08 10:01:09 +0100

    store all nodes

--- 2023/day08/day08.zig
+++ 2023/day08/day08.zig
@@ -6,10 +6,37 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    _ = allocator;
-    _ = text;
+    var nodes = std.ArrayList(Node).init(allocator);
+    defer nodes.deinit();
+
+    var lines_it = utils.tokenize(text, "\r\n");
+    const instructions = lines_it.next().?;
+    _ = instructions;
+    while (lines_it.next()) |line| {
+        var line_it = utils.tokenize(line, " =(,)");
+        const from = line_it.next().?;
+        const left = line_it.next().?;
+        const right = line_it.next().?;
+
+        try nodes.append(.{
+            .from = from,
+            .left = left,
+            .right = right,
+        });
+    }
+
+    for (nodes.items) |node| {
+        std.debug.print("{s}: <- {s}, -> {s}\n", .{ node.from, node.left, node.right });
+    }
+
     return 0;
 }
+
+const Node = struct {
+    from: []const u8,
+    left: []const u8,
+    right: []const u8,
+};
 
 test "example01" {
     const text = @embedFile("example01.txt");
