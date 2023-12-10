c36457718883ae833671cc9ba35ff4de56410a0a
GigaGrunch <gigagrunch@gmail.com>
2023-12-10 12:39:26 +0100

    calculate farthest distance in loop

--- 2023/day10/day10.zig
+++ 2023/day10/day10.zig
@@ -25,17 +25,106 @@ fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
     };
 
     var start: Coord = undefined;
-    for (0..height) |y| {
+    var current_1: ?Coord = null;
+    var current_2: ?Coord = null;
+
+    outer: for (0..height) |y| {
         for (0..width) |x| {
-            if (map.at(x, y) == 'S') {
-                start = .{ .x = x, .y = y };
+            start = .{ .x = x, .y = y };
+            if (map.at(start) == 'S') {
+                if (x > 0) {
+                    const left = Coord { .x = x - 1, .y = y };
+                    switch (map.at(left)) {
+                        '-', 'F', 'L' => {
+                            if (current_1 == null) {
+                                current_1 = left;
+                            } else if (current_2 == null) {
+                                current_2 = left;
+                            }
+                        },
+                        else => { },
+                    }
+                }
+                if (x < width) {
+                    const right = Coord { .x = x + 1, .y = y };
+                    switch (map.at(right)) {
+                        '-', '7', 'J' => {
+                            if (current_1 == null) {
+                                current_1 = right;
+                            } else if (current_2 == null) {
+                                current_2 = right;
+                            }
+                        },
+                        else => { },
+                    }
+                }
+                if (y > 0) {
+                    const up = Coord { .x = x, .y = y - 1 };
+                    switch (map.at(up)) {
+                        '|', 'F', '7' => {
+                            if (current_1 == null) {
+                                current_1 = up;
+                            } else if (current_2 == null) {
+                                current_2 = up;
+                            }
+                        },
+                        else => { },
+                    }
+                }
+                if (y < height) {
+                    const down = Coord { .x = x, .y = y + 1 };
+                    switch (map.at(down)) {
+                        '|', 'J', 'L' => {
+                            if (current_1 == null) {
+                                current_1 = down;
+                            } else if (current_2 == null) {
+                                current_2 = down;
+                            }
+                        },
+                        else => { },
+                    }
+                }
+
+                break :outer;
             }
         }
     }
 
-    std.debug.print("{}\n", .{start});
+    std.debug.assert(current_1 != null and current_2 != null);
 
-    return 0;
+    var visited = std.ArrayList(Coord).init(allocator);
+    defer visited.deinit();
+
+    try visited.append(start);
+
+    while (current_1 != null or current_2 != null) {
+        if (current_1) |c_1| {
+            try visited.append(c_1);
+            current_1 = null;
+            const connections = map.findConnections(c_1);
+            if (!utils.containsItem(visited.items, connections._1)) {
+                current_1 = connections._1;
+            }
+            else if (!utils.containsItem(visited.items, connections._2)) {
+                current_1 = connections._2;
+            }
+        }
+        if (current_2) |c_2| {
+            try visited.append(c_2);
+            current_2 = null;
+            const connections = map.findConnections(c_2);
+            if (!utils.containsItem(visited.items, connections._1)) {
+                current_2 = connections._1;
+            }
+            else if (!utils.containsItem(visited.items, connections._2)) {
+                current_2 = connections._2;
+            }
+        }
+    }
+
+    const loop_length: i32 = @intCast(visited.items.len);
+    const farthest_distance = @divTrunc(loop_length, 2);
+    return farthest_distance;
 }
 
 const Coord = struct {
@@ -48,8 +137,20 @@ const Map = struct {
     width: usize,
     height: usize,
 
-    fn at(self: Map, x: usize, y: usize) u8 {
-        return self.string[y * self.width + x];
+    fn at(self: Map, coord: Coord) u8 {
+        return self.string[coord.y * self.width + coord.x];
+    }
+
+    fn findConnections(self: Map, coord: Coord) struct { _1: Coord, _2: Coord, } {
+        return switch (self.at(coord)) {
+            '|' => .{ ._1 = .{ .x = coord.x, .y = coord.y - 1 }, ._2 = .{ .x = coord.x, .y = coord.y + 1 } },
+            '-' => .{ ._1 = .{ .x = coord.x - 1, .y = coord.y }, ._2 = .{ .x = coord.x + 1, .y = coord.y } },
+            'F' => .{ ._1 = .{ .x = coord.x, .y = coord.y + 1 }, ._2 = .{ .x = coord.x + 1, .y = coord.y } },
+            'J' => .{ ._1 = .{ .x = coord.x - 1, .y = coord.y }, ._2 = .{ .x = coord.x, .y = coord.y - 1 } },
+            '7' => .{ ._1 = .{ .x = coord.x - 1, .y = coord.y }, ._2 = .{ .x = coord.x, .y = coord.y + 1 } },
+            'L' => .{ ._1 = .{ .x = coord.x, .y = coord.y - 1 }, ._2 = .{ .x = coord.x + 1, .y = coord.y } },
+            else => unreachable,
+        };
     }
 };
 
