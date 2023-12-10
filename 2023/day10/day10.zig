ccb39630e52389d159a8712ad90b57f29b31158d
GigaGrunch <gigagrunch@gmail.com>
2023-12-10 12:00:20 +0100

    parse and print map

--- 2023/day10/day10.zig
+++ 2023/day10/day10.zig
@@ -6,10 +6,38 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    _ = text;
-    _ = allocator;
+    var string = std.ArrayList(u8).init(allocator);
+    defer string.deinit();
+
+    var width: usize = undefined;
+    var height: usize = 0;
+    var lines_it = utils.tokenize(text, "\r\n");
+    while (lines_it.next()) |line| {
+        width = line.len;
+        height += 1;
+        try string.appendSlice(line);
+    }
+
+    const map = Map {
+        .string = string.items,
+        .width = width,
+        .height = height,
+    };
+
+    for (0..height) |y| {
+        const start = y * width;
+        const end = start + width;
+        std.debug.print("{s}\n", .{map.string[start..end]});
+    }
+
     return 0;
 }
+
+const Map = struct {
+    string: []const u8,
+    width: usize,
+    height: usize,  
+};
 
 test "example01" {
     const text = @embedFile("example01.txt");
