80d2f9ed51a95889542222d147a8872734168977
GigaGrunch <gigagrunch@gmail.com>
2023-12-11 10:31:19 +0100

    parse map dimensions and galaxy positions

--- 2023/day11/day11.zig
+++ 2023/day11/day11.zig
@@ -6,10 +6,34 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    _ = allocator;
-    _ = text;
+    var width: i32 = 0;
+    var height: i32 = 0;
+
+    var galaxies = std.ArrayList(Pos).init(allocator);
+    defer galaxies.deinit();
+
+    var lines_it = utils.tokenize(text, "\r\n");
+    while (lines_it.next()) |line| {
+        width = @intCast(line.len);
+        height += 1;
+
+        for (line, 0..) |char, x| {
+            if (char == '#') try galaxies.append(.{ .x = @intCast(x), .y = height });
+        }
+    }
+
+    std.debug.print("width: {d}, height: {d}\n", .{ width, height });
+    std.debug.print("galaxies: ", .{});
+    for (galaxies.items) |pos| std.debug.print("({d},{d}) ", .{ pos.x, pos.y });
+    std.debug.print("\n", .{});
+
     return 0;
 }
+
+const Pos = struct {
+    x: i32,
+    y: i32,
+};
 
 test {
     const text = @embedFile("example.txt");
