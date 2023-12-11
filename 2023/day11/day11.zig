7c0d9312aa993165694e97e2e5e2b20def0b2c46
GigaGrunch <gigagrunch@gmail.com>
2023-12-11 10:37:19 +0100

    parse empty rows and columns

--- 2023/day11/day11.zig
+++ 2023/day11/day11.zig
@@ -6,19 +6,39 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    var width: i32 = 0;
-    var height: i32 = 0;
-
     var galaxies = std.ArrayList(Pos).init(allocator);
-    defer galaxies.deinit();
+    var empty_rows = std.ArrayList(i32).init(allocator);
+    var empty_columns = std.ArrayList(i32).init(allocator);
+    defer {
+        galaxies.deinit();
+        empty_rows.deinit();
+        empty_columns.deinit();
+    }
+
+    var width: usize = 0;
+    var height: usize = 0;
 
     var lines_it = utils.tokenize(text, "\r\n");
     while (lines_it.next()) |line| {
-        width = @intCast(line.len);
+        width = line.len;
         height += 1;
 
+        var is_empty_row = true;
         for (line, 0..) |char, x| {
-            if (char == '#') try galaxies.append(.{ .x = @intCast(x), .y = height });
+            if (char == '#') {
+                try galaxies.append(.{ .x = @intCast(x), .y = @intCast(height) });
+                is_empty_row = false;
+            }
+        }
+
+        if (is_empty_row) try empty_rows.append(@intCast(height));
+    }
+
+    for (0..width) |x| {
+        if (for (galaxies.items) |pos| {
+            if (pos.x == x) break false;
+        } else true) {
+            try empty_columns.append(@intCast(x));
         }
     }
 
@@ -26,6 +46,8 @@ fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
     std.debug.print("galaxies: ", .{});
     for (galaxies.items) |pos| std.debug.print("({d},{d}) ", .{ pos.x, pos.y });
     std.debug.print("\n", .{});
+    std.debug.print("empty rows: {any}\n", .{empty_rows.items});
+    std.debug.print("empty columns: {any}\n", .{empty_columns.items});
 
     return 0;
 }
