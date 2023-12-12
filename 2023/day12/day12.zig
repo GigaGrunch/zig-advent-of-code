deeae9aff5a26ff49b5a1447980ccf0e78228b4b
GigaGrunch <gigagrunch@gmail.com>
2023-12-12 10:06:21 +0100

    parse input

--- 2023/day12/day12.zig
+++ 2023/day12/day12.zig
@@ -6,8 +6,25 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    _ = allocator;
-    _ = text;
+    var lines_it = utils.tokenize(text, "\r\n");
+    while (lines_it.next()) |line| {
+        var line_it = utils.tokenize(line, " ");
+        const spring_list = line_it.next().?;
+
+        var groups_list = std.ArrayList(i32).init(allocator);
+        defer groups_list.deinit();
+
+        var groups_it = utils.tokenize(line_it.next().?, ",");
+        while (groups_it.next()) |group_string| {
+            const group = try utils.parseInt(i32, group_string);
+            try groups_list.append(group);
+        }
+
+        const groups = groups_list.items;
+
+        std.debug.print("{s} {any}\n", .{ spring_list, groups });
+    }
+
     return 0;
 }
 
