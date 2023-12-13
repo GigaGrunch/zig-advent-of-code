0aabb5f8df705f3d8391c59c9471ea44c2d8e6ab
GigaGrunch <gigagrunch@gmail.com>
2023-12-13 14:50:27 +0100

    apply smudge until other match is found

--- 2023/day13/day13.zig
+++ 2023/day13/day13.zig
@@ -12,38 +12,84 @@ fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
 
     var patterns_it = std.mem.split(u8, text, empty_line);
     while (patterns_it.next()) |pattern| {
-        var lines = std.ArrayList([]const u8).init(allocator);
-        defer lines.deinit();
+        var lines = std.ArrayList([]u8).init(allocator);
+        defer {
+            for (lines.items) |line| allocator.free(line);
+            lines.deinit();
+        }
 
         var lines_it = utils.tokenize(pattern, "\r\n");
-        while (lines_it.next()) |line| {
+        while (lines_it.next()) |src_line| {
+            var line = try allocator.alloc(u8, src_line.len);
+            std.mem.copy(u8, line, src_line);
             try lines.append(line);
         }
 
-        var horizontal_mirror_candidates = std.ArrayList(bool).init(allocator);
-        defer horizontal_mirror_candidates.deinit();
-        try horizontal_mirror_candidates.append(false);
+        const original_vertical_reflection_line = try findVerticalReflectionLine(lines.items, allocator);
+        const original_horizontal_reflection_line = try findHorizontalReflectionLine(lines.items, allocator);
 
-        for (lines.items) |line| {
-            try updateReflectionCandidates(u8, line, &horizontal_mirror_candidates);
-        }
+        outer: for (0..lines.items.len) |smudge_y| {
+            for (0..lines.items[0].len) |smudge_x| {
+                var smudge = &lines.items[smudge_y][smudge_x];
+                flipSmudge(smudge);
+                defer flipSmudge(smudge);
 
-        var vertical_mirror_candidates = std.ArrayList(bool).init(allocator);
-        defer vertical_mirror_candidates.deinit();
-        try vertical_mirror_candidates.append(false);
+                if (try findVerticalReflectionLine(lines.items, allocator)) |vertical_line| {
+                    if (vertical_line != original_vertical_reflection_line) {
+                        sum += @intCast(vertical_line);
+                        break :outer;
+                    }
+                }
 
-        try updateReflectionCandidates([]const u8, lines.items, &vertical_mirror_candidates);
-
-        if (std.mem.indexOfScalar(bool, horizontal_mirror_candidates.items, true)) |index| {
-            sum += @intCast(index);
-        } else if (std.mem.indexOfScalar(bool, vertical_mirror_candidates.items, true)) |index| {
-            sum += @intCast(100 * index);
-        } else {
-            unreachable;
+                if (try findHorizontalReflectionLine(lines.items, allocator)) |horizontal_line| {
+                    if (horizontal_line != original_horizontal_reflection_line) {
+                        sum += @intCast(100 * horizontal_line);
+                        break :outer;
+                    }
+                }
+            }
         }
     }
 
     return sum;
+}
+
+fn findVerticalReflectionLine(lines: []const []const u8, allocator: std.mem.Allocator) !?usize {
+    var vertical_line_candidates = std.ArrayList(bool).init(allocator);
+    defer vertical_line_candidates.deinit();
+    try vertical_line_candidates.append(false);
+
+    for (lines) |line| {
+        try updateReflectionCandidates(u8, line, &vertical_line_candidates);
+    }
+
+    if (std.mem.indexOfScalar(bool, vertical_line_candidates.items, true)) |index| {
+        return index;
+    }
+
+    return null;
+}
+
+fn findHorizontalReflectionLine(lines: []const []const u8, allocator: std.mem.Allocator) !?usize {
+    var horizontal_line_candidates = std.ArrayList(bool).init(allocator);
+    defer horizontal_line_candidates.deinit();
+    try horizontal_line_candidates.append(false);
+
+    try updateReflectionCandidates([]const u8, lines, &horizontal_line_candidates);
+
+    if (std.mem.indexOfScalar(bool, horizontal_line_candidates.items, true)) |index| {
+        return index;
+    }
+
+    return null;
+}
+
+fn flipSmudge(smudge: *u8) void {
+    smudge.* = switch (smudge.*) {
+        '#' => '.',
+        '.' => '#',
+        else => unreachable,
+    };
 }
 
 fn updateReflectionCandidates(comptime T: type, list: []const T, candidates: *std.ArrayList(bool)) !void {
@@ -78,7 +124,7 @@ fn updateReflectionCandidates(comptime T: type, list: []const T, candidates: *st
 
 test {
     const text = @embedFile("example.txt");
-    const expected: i32 = 405;
+    const expected: i32 = 400;
     const result = try execute(text, std.testing.allocator);
     try std.testing.expectEqual(expected, result);
 }
