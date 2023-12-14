d6485562f78203198778988a3758273dbbc9d97e
GigaGrunch <gigagrunch@gmail.com>
2023-12-14 11:51:24 +0100

    complete one cycle

--- 2023/day14/day14.zig
+++ 2023/day14/day14.zig
@@ -24,6 +24,10 @@ fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
         .height = height,
     };
 
+    std.debug.print("\ninitial:\n\n", .{});
+    map.print();
+
+    // north
     for (0..map.height) |y| {
         for (0..map.width) |x| {
             if (map.get(x, y) == 'O') {
@@ -36,6 +40,62 @@ fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
             }
         }
     }
+
+    std.debug.print("\nnorth:\n\n", .{});
+    map.print();
+
+    // east
+    for (0..map.width) |x| {
+        for (0..map.height) |y| {
+            if (map.get(x, y) == 'O') {
+                var other_x = x;
+                while (other_x > 0) : (other_x -= 1) {
+                    if (map.get(other_x - 1, y) != '.') break;
+                    map.set(other_x, y, '.');
+                    map.set(other_x - 1, y, 'O');
+                }
+            }
+        }
+    }
+
+    std.debug.print("\neast:\n\n", .{});
+    map.print();
+
+    // south
+    for (0..map.height) |inv_y| {
+        const y = map.height - 1 - inv_y;
+        for (0..map.width) |x| {
+            if (map.get(x, y) == 'O') {
+                var other_y = y;
+                while (other_y < map.height - 1) : (other_y += 1) {
+                    if (map.get(x, other_y + 1) != '.') break;
+                    map.set(x, other_y, '.');
+                    map.set(x, other_y + 1, 'O');
+                }
+            }
+        }
+    }
+
+    std.debug.print("\nsouth:\n\n", .{});
+    map.print();
+
+    // west
+    for (0..map.width) |inv_x| {
+        const x = map.width - 1 - inv_x;
+        for (0..map.height) |y| {
+            if (map.get(x, y) == 'O') {
+                var other_x = x;
+                while (other_x < map.width - 1) : (other_x += 1) {
+                    if (map.get(other_x + 1, y) != '.') break;
+                    map.set(other_x, y, '.');
+                    map.set(other_x + 1, y, 'O');
+                }
+            }
+        }
+    }
+
+    std.debug.print("\nwest:\n\n", .{});
+    map.print();
 
     var load: i32 = 0;
 
@@ -79,7 +139,7 @@ const Map = struct {
 
 test {
     const text = @embedFile("example.txt");
-    const expected: i32 = 136;
+    const expected: i32 = 64;
     const result = try execute(text, std.testing.allocator);
     try std.testing.expectEqual(expected, result);
 }
