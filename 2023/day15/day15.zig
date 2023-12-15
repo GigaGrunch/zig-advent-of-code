fae6d6b01d2bcbcd0dd1b69e4bc002f9efe86177
GigaGrunch <gigagrunch@gmail.com>
2023-12-15 16:01:56 +0100

    calculate string hashes (finish part 1)

--- 2023/day15/day15.zig
+++ 2023/day15/day15.zig
@@ -7,8 +7,25 @@ pub fn main() !void {
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
     _ = allocator;
-    _ = text;
-    return 0;
+
+    var sum: i32 = 0;
+
+    var string_it = utils.tokenize(text, ",\r\n");
+    while (string_it.next()) |string| {
+        sum += calculateHash(string);
+    }
+
+    return sum;
+}
+
+fn calculateHash(string: []const u8) u8 {
+    var hash: i32 = 0;
+    for (string) |char| {
+        hash += char;
+        hash *= 17;
+        hash = @mod(hash, 256);
+    }
+    return @intCast(hash);
 }
 
 test "HASH" {
