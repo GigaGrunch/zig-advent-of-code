3d101b0a18c81137fe29bbf6e28c6f48aac60f98
GigaGrunch <gigagrunch@gmail.com>
2023-12-17 15:39:00 +0100

    parse the map

--- 2023/day17/day17.zig
+++ 2023/day17/day17.zig
@@ -6,10 +6,34 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    _ = allocator;
-    _ = text;
+    var map_list = std.ArrayList(i32).init(allocator);
+    defer map_list.deinit();
+    height = 0;
+
+    var lines_it = utils.tokenize(text, "\r\n");
+    while (lines_it.next()) |line| {
+        height += 1;
+
+        for (line) |char| {
+            try map_list.append(char - '0');
+        }
+    }
+
+    map = map_list.items;
+    width = map.len / height;
+
+    for (0..height) |y| {
+        const start = y * width;
+        const end = start + width;
+        std.debug.print("{any}\n", .{map[start..end]});
+    }
+
     return 0;
 }
+
+var map: []const i32 = undefined;
+var width: usize = undefined;
+var height: usize = undefined;
 
 test {
     const text = @embedFile("example.txt");
