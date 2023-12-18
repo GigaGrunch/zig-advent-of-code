f2eafe30055265e18243d19a77005b195ab355b6
GigaGrunch <gigagrunch@gmail.com>
2023-12-18 09:45:19 +0100

    draw the edge of the lagoon

--- 2023/day18/day18.zig
+++ 2023/day18/day18.zig
@@ -6,10 +6,66 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    _ = allocator;
-    _ = text;
+    var visited_positions = std.ArrayList(Pos).init(allocator);
+    defer visited_positions.deinit();
+
+    var current_pos = Pos{ .x = 0, .y = 0 };
+    try visited_positions.append(current_pos);
+
+    var lines_it = utils.tokenize(text, "\r\n");
+    while (lines_it.next()) |line| {
+        var line_it = utils.tokenize(line, " ");
+        const dir = line_it.next().?[0];
+        const count = try utils.parseInt(usize, line_it.next().?);
+
+        for (0..count) |_| {
+            switch (dir) {
+                'U' => current_pos.y -= 1,
+                'D' => current_pos.y += 1,
+                'L' => current_pos.x -= 1,
+                'R' => current_pos.x += 1,
+                else => unreachable,
+            }
+
+            try visited_positions.append(current_pos);
+        }
+    }
+
+    var top_left = Pos{
+        .x = std.math.maxInt(usize),
+        .y = std.math.maxInt(usize),
+    };
+    var bottom_right = Pos{
+        .x = std.math.minInt(usize),
+        .y = std.math.minInt(usize),
+    };
+
+    for (visited_positions.items) |pos| {
+        top_left.x = @min(top_left.x, pos.x);
+        top_left.y = @min(top_left.y, pos.y);
+        bottom_right.x = @max(bottom_right.x, pos.x);
+        bottom_right.y = @max(bottom_right.y, pos.y);
+    }
+
+    std.debug.print("initial map:\n", .{});
+    for (top_left.y..bottom_right.y + 1) |y| {
+        for (top_left.x..bottom_right.x + 1) |x| {
+            if (utils.containsItem(visited_positions.items, Pos{ .x = x, .y = y })) {
+                std.debug.print("#", .{});
+            } else {
+                std.debug.print(".", .{});
+            }
+        }
+        std.debug.print("\n", .{});
+    }
+
     return 0;
 }
+
+const Pos = struct {
+    x: usize,
+    y: usize,
+};
 
 test {
     const text = @embedFile("example.txt");
