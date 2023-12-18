89c987e0a3f88f4097ac5e7df223bd5fd4eb202e
GigaGrunch <gigagrunch@gmail.com>
2023-12-18 15:04:52 +0100

    just store edges instead of map

--- 2023/day18/day18.zig
+++ 2023/day18/day18.zig
@@ -6,206 +6,67 @@ pub fn main() !void {
 }
 
 fn execute(text: []const u8, allocator: std.mem.Allocator) !i32 {
-    var visited_positions = std.ArrayList(Pos).init(allocator);
-    defer visited_positions.deinit();
+    var horizontal_edges = std.ArrayList(HorizontalEdge).init(allocator);
+    var vertical_edges = std.ArrayList(VerticalEdge).init(allocator);
+    defer {
+        horizontal_edges.deinit();
+        vertical_edges.deinit();
+    }
 
-    var current_pos = Pos{ .x = 0, .y = 0 };
-    try visited_positions.append(current_pos);
+    {
+        var pos = Pos{ .x = 0, .y = 0 };
 
-    var lines_it = utils.tokenize(text, "\r\n");
-    while (lines_it.next()) |line| {
-        var line_it = utils.tokenize(line, " ");
-        const dir = line_it.next().?[0];
-        const count = try utils.parseInt(usize, line_it.next().?);
+        var lines_it = utils.tokenize(text, "\r\n");
+        while (lines_it.next()) |line| {
+            var line_it = utils.tokenize(line, " ");
+            const dir = line_it.next().?[0];
+            const count = try utils.parseInt(i32, line_it.next().?);
 
-        for (0..count) |_| {
             switch (dir) {
-                'U' => current_pos.y -= 1,
-                'D' => current_pos.y += 1,
-                'L' => current_pos.x -= 1,
-                'R' => current_pos.x += 1,
+                'U' => {
+                    pos.y -= count - 1;
+                    try vertical_edges.append(.{ .start = pos, .length = count });
+                    pos.y -= 1;
+                },
+                'D' => {
+                    try vertical_edges.append(.{ .start = pos, .length = count });
+                    pos.y += count;
+                },
+                'L' => {
+                    pos.x -= count - 1;
+                    try horizontal_edges.append(.{ .start = pos, .length = count });
+                    pos.x -= 1;
+                },
+                'R' => {
+                    try horizontal_edges.append(.{ .start = pos, .length = count });
+                    pos.x += count;
+                },
                 else => unreachable,
             }
-
-            try visited_positions.append(current_pos);
         }
     }
 
-    var top_left = Pos{
-        .x = std.math.maxInt(i32),
-        .y = std.math.maxInt(i32),
-    };
-    var bottom_right = Pos{
-        .x = std.math.minInt(i32),
-        .y = std.math.minInt(i32),
-    };
+    std.debug.print("horizontal edges:\n", .{});
+    for (horizontal_edges.items) |edge| std.debug.print("{}\n", .{edge});
+    std.debug.print("vertical edges:\n", .{});
+    for (vertical_edges.items) |edge| std.debug.print("{}\n", .{edge});
 
-    for (visited_positions.items) |pos| {
-        top_left.x = @min(top_left.x, pos.x);
-        top_left.y = @min(top_left.y, pos.y);
-        bottom_right.x = @max(bottom_right.x, pos.x);
-        bottom_right.y = @max(bottom_right.y, pos.y);
-    }
-
-    const width: usize = @intCast(1 + bottom_right.x - top_left.x);
-    const height: usize = @intCast(1 + bottom_right.y - top_left.y);
-
-    var map = std.ArrayList(Tile).init(allocator);
-    defer map.deinit();
-
-    for (0..height) |y| {
-        for (0..width) |x| {
-            const x_pos = @as(i32, @intCast(x)) + top_left.x;
-            const y_pos = @as(i32, @intCast(y)) + top_left.y;
-            if (utils.containsItem(visited_positions.items, Pos{ .x = x_pos, .y = y_pos })) {
-                try map.append(.Edge);
-            } else {
-                try map.append(.Unknown);
-            }
-        }
-    }
-
-    std.debug.print("outline:\n", .{});
-    for (0..height) |y| {
-        for (0..width) |x| {
-            const index = y * width + x;
-            switch (map.items[index]) {
-                .Edge, .Inner => std.debug.print("#", .{}),
-                .Unknown, .Outer => std.debug.print(".", .{}),
-            }
-        }
-        std.debug.print("\n", .{});
-    }
-
-    while (true) {
-        for (0..height) |y| {
-            for (0..width) |x| {
-                const index = y * width + x;
-
-                if (map.items[index] != .Unknown) continue;
-
-                if (x == 0 or y == 0) {
-                    map.items[index] = .Outer;
-                    continue;
-                }
-                if (x > 0) {
-                    const prev = map.items[index - 1];
-                    if (prev != .Unknown and prev != .Edge) {
-                        map.items[index] = prev;
-                        continue;
-                    }
-                }
-                if (x == 1) {
-                    const prev = map.items[index - 1];
-                    if (prev == .Edge) {
-                        map.items[index] = .Inner;
-                        continue;
-                    }
-                }
-                if (y > 0) {
-                    const prev = map.items[index - width];
-                    if (prev != .Unknown and prev != .Edge) {
-                        map.items[index] = prev;
-                        continue;
-                    }
-                }
-                if (y == 1) {
-                    const prev = map.items[index - width];
-                    if (prev == .Edge) {
-                        map.items[index] = .Inner;
-                        continue;
-                    }
-                }
-            }
-        }
-
-        for (0..height) |inv_y| {
-            for (0..width) |inv_x| {
-                const x = width - 1 - inv_x;
-                const y = height - 1 - inv_y;
-
-                const index = y * width + x;
-
-                if (map.items[index] != .Unknown) continue;
-
-                if (x == width - 1 or y == height - 1) {
-                    map.items[index] = .Outer;
-                    continue;
-                }
-                if (x < width - 1) {
-                    const next = map.items[index + 1];
-                    if (next != .Unknown and next != .Edge) {
-                        map.items[index] = next;
-                        continue;
-                    }
-                }
-                if (x == width - 2) {
-                    const next = map.items[index + 1];
-                    if (next == .Edge) {
-                        map.items[index] = .Inner;
-                        continue;
-                    }
-                }
-                if (y < height - 1) {
-                    const next = map.items[index + width];
-                    if (next != .Unknown and next != .Edge) {
-                        map.items[index] = next;
-                        continue;
-                    }
-                }
-                if (y == height - 2) {
-                    const next = map.items[index + width];
-                    if (next == .Edge) {
-                        map.items[index] = .Inner;
-                        continue;
-                    }
-                }
-            }
-        }
-
-        var unknown_count: i32 = 0;
-        for (0..height) |y| {
-            for (0..width) |x| {
-                const index = y * width + x;
-                if (map.items[index] == .Unknown) {
-                    unknown_count += 1;
-                }
-            }
-        }
-
-        if (unknown_count == 0) break;
-    }
-
-    var count: i32 = 0;
-
-    std.debug.print("lagoon:\n", .{});
-    for (0..height) |y| {
-        for (0..width) |x| {
-            const index = y * width + x;
-            switch (map.items[index]) {
-                .Edge, .Inner => {
-                    std.debug.print("#", .{});
-                    count += 1;
-                },
-                .Unknown, .Outer => std.debug.print(".", .{}),
-            }
-        }
-        std.debug.print("\n", .{});
-    }
-
-    return count;
+    return 0;
 }
+
+const HorizontalEdge = struct {
+    start: Pos,
+    length: i32,
+};
+
+const VerticalEdge = struct {
+    start: Pos,
+    length: i32,
+};
 
 const Pos = struct {
     x: i32,
     y: i32,
-};
-
-const Tile = enum {
-    Edge,
-    Unknown,
-    Outer,
-    Inner,
 };
 
 test {
